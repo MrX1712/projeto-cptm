3523196c296dc17476a867c76005da98f8c53bf6
Martim Roxo <martimroxo6@gmail.com>
2025-06-04 09:03:20 -0300

    controller não funcional

--- cptm/src/main/java/br/edu/ibmec/cptm/controller/EstacaoController.java
+++ cptm/src/main/java/br/edu/ibmec/cptm/controller/EstacaoController.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.UUID;
-import java.util.List;
 
 @Controller
 @RequestMapping("/cptm+/adm/painel-administrativo/estacoes")
