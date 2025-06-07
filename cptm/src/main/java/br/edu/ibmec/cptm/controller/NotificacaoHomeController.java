e062bf141e6fbcc10d67e5f6abafb1eec6fde5fe
Martim Roxo <martimroxo6@gmail.com>
2025-06-07 13:23:51 -0300

    painel administrativo completo

---
+++ cptm/src/main/java/br/edu/ibmec/cptm/controller/NotificacaoHomeController.java
@@ -0,0 +1,23 @@
+package br.edu.ibmec.cptm.controller;
+
+
+import br.edu.ibmec.cptm.service.NotificacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cptm+/home")
+public class NotificacaoHomeController {
+
+    @Autowired
+    private NotificacaoService notificacaoService;
+
+    @GetMapping("/")
+    public String notificacoesHome(Model model) {
+        model.addAttribute("notificacoes", notificacaoService.listar());
+        return "cptm/home";
+    }
+}
